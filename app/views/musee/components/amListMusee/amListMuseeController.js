0fd0482b9ae28a17155547a8209aeafa95917d25
Sébastien LE MOUILLOUR <dev.n2ofr@gmail.com>
2015-05-31 02:50:28 +0200

    reconnexion avec la partie back-end

--- app/views/musee/components/amListMusee/amListMuseeController.js
+++ app/views/musee/components/amListMusee/amListMuseeController.js
@@ -2,11 +2,6 @@ angular.module('AnnuaireMuseeApp').controller('AmListMuseeController',
     function (AmListMuseeService) {
 
         var vm = this;
-        vm.listMusee = [
-            {"id":"1","nom":"Mus\u00e9e 1","description":"description mus\u00e9e 1","categories":[]},
-            {"id":"2","nom":"Mus\u00e9e 2","description":"description mus\u00e9e 2","categories":[]},
-            {"id":"3","nom":"testAddMusee2","description":"testAddMusee","categories":[]},
-            {"id":"4","nom":"testAddMusee2","description":"testAddMuseeAAA","categories":[]}];
 
         vm.gridOptions = {
         };
@@ -17,14 +12,14 @@ angular.module('AnnuaireMuseeApp').controller('AmListMuseeController',
             {name:'description'}, // showing backwards compatibility with 2.x.  you can use field in place of name
             {name: 'categories'}
         ];
-        vm.gridOptions.data = vm.listMusee;
+
 
 
 
         vm.getListMusee = function () {
             AmListMuseeService.getListMusee()
                 .then(function(musees) {
-                    vm.listMusee = musees;
+                    vm.gridOptions.data = musees;
                     console.log('musees returned to controller.');
                 },
                 function(data) {
@@ -32,6 +27,6 @@ angular.module('AnnuaireMuseeApp').controller('AmListMuseeController',
                 });
         };
 
-        //vm.getListMusee();
+        vm.getListMusee();
     }
 );
